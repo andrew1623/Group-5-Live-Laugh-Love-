895f6537a414c0e714144080afe1cc2957b73ba9
AT <act.webdesigns@gmail.com>
2023-03-08 19:10:09 -0600

    Initiated App
    initiated/set up a react app w/ tailwindcss

---
+++ app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className='App'>
+        <div className='flex items-center items-top justify-between'>
+        <h1 className='text-3xl font-bold underline'>Text Adventures</h1>
+          <div className='buttons'>
+            <button className='button'>Home</button>
+            <button className='button'>Set User</button>
+          </div>
+      </div>
+
+      <div>
+        <p>this is where the rest will go</p>
+      </div>
+    </div>
+
+
+  )
+}
+
+export default App
