fd6fbdfa51e759445bbcbc8e06876213083c55a7
OliviaCantrell <123116908+OliviaCantrell@users.noreply.github.com>
2023-03-30 13:10:23 -0500

    Update tailwind.config.cjs

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -9,6 +9,29 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["luxury"]
-  }
+themes: [
+        {
+          mytheme: {
+          
+ "primary": "#4c8dc2",
+          
+ "secondary": "#c24c4c",
+          
+ "accent": "#AACde9",
+          
+ "neutral": "#3D4451",
+          
+ "base-100": "#104978",
+          
+ "info": "#77adda",
+          
+ "success": "#296699e",
+          
+ "warning": "#9e2929",
+          
+ "error": "#781010",
+          },
+        },
+      ],
+    },
 };
